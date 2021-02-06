095a536ea851d1767143f5e92721cd5cab8352e1
t-kubrak <chikashikaari@gmail.com>
2021-02-06 21:09:04 +0200

    feat: add initial page layout and form

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,8 @@ import { handleSubmit } from './js/app'
 
 import './styles/resets.scss'
 import './styles/base.scss'
+import './styles/header.scss'
+import './styles/form.scss'
 
 export {
     handleSubmit
