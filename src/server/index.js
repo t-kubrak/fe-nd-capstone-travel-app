7cb826193d36e551eea60e91d1469df4d6b0e25b
t-kubrak <chikashikaari@gmail.com>
2021-02-07 19:49:56 +0200

    feat: show forecast

--- src/server/index.js
+++ src/server/index.js
@@ -27,10 +27,11 @@ app.listen(8081, function () {
 
 app.post('/trip', async function (req, res) {
     const city = req.body.city;
-    const date = req.body.date;
+    let date = req.body.date;
     const geoNamesEndpoint = `http://api.geonames.org/searchJSON?username=${process.env.GEONAMES_API_USERNAME}&q=${city}&maxRows=1`;
 
     try {
+        // Get coordinates
         const geoNamesRes = await fetch(encodeURI(geoNamesEndpoint), {
             method: 'GET',
         });
@@ -38,15 +39,57 @@ app.post('/trip', async function (req, res) {
         const lng = jsonRes.geonames[0].lng;
         const lat = jsonRes.geonames[0].lat;
 
-        const weatherbitEndpoint = `http://api.weatherbit.io/v2.0/current?&key=${process.env.WEATHERBIT_API_KEY}&lat=${lat}&lon=${lng}`;
+        // Show current weather if trip date is withing a week,
+        // otherwise get daily forecast for 16 days
+        let dateForNextWeek = new Date();
+        const nextWeekDate = (new Date()).getDate()+7;
+        dateForNextWeek.setDate(nextWeekDate);
+
+        const isTripWithinWeek = dateForNextWeek.getTime() >= (new Date(date)).getTime();
+        const forecast = isTripWithinWeek ? 'current' : 'forecast/daily';
+
+        const weatherbitEndpoint = `http://api.weatherbit.io/v2.0/${forecast}?&key=${process.env.WEATHERBIT_API_KEY}&lat=${lat}&lon=${lng}`;
 
         const weatherbitRes = await fetch(encodeURI(weatherbitEndpoint), {
             method: 'GET',
         });
         jsonRes = await weatherbitRes.json();
-        const weatherDesc = jsonRes.data[0].weather.description;
-        const temperature = jsonRes.data[0].temp;
 
+        let weatherDesc = '';
+        let temperature = '';
+
+        // Show current weather
+        if (isTripWithinWeek) {
+            console.log('Current weather:')
+            weatherDesc = jsonRes.data[0].weather.description;
+            temperature = jsonRes.data[0].temp;
+        } else {
+            const dailyForecast = jsonRes.data;
+
+            // Compare dates and show the forecast for correct one
+            for (const day of dailyForecast) {
+                const isTripDay = (new Date(day.datetime)).getTime() === (new Date(date)).getTime();
+                console.log(isTripDay);
+                if (isTripDay) {
+                    console.log(`Forecast for day: ${day.datetime}`);
+                    date = day.datetime;
+                    weatherDesc = day.weather.description;
+                    temperature = day.temp;
+                    break;
+                }
+            }
+
+            // If date is not within 16 days range(API limitations), that just show the last one
+            if (!weatherDesc) {
+                const lastDayForecast = dailyForecast[dailyForecast.length - 1];
+                console.log(`Latest possible forecast for day: ${lastDayForecast.datetime}`);
+                date = lastDayForecast.datetime;
+                weatherDesc = lastDayForecast.weather.description;
+                temperature = lastDayForecast.temp;
+            }
+        }
+
+        // Get image url
         const pixabayEndpoint = `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${city}`
         const pixabayRes = await fetch(encodeURI(pixabayEndpoint), {
             method: 'GET',
